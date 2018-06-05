abad89dae94cbe57c4ad8d00d2299838a431cd03
gfoo <gilles.faucherand@gmail.com>
2018-06-05 12:25:40 +0200

    jsonld poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, mergeAll, toArray } from 'rxjs/operators';
+import { environment } from '../environments/environment';
+
+export class Thing {
+  id: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  resources$: Observable<Thing[]>;
+
+  gravQuery = `
+PREFIX knora-api: <http://api.knora.org/ontology/knora-api/simple/v2#>
+PREFIX any: <http://0.0.0.0:3333/ontology/0001/anything/simple/v2#>
+CONSTRUCT {
+  ?s knora-api:isMainResource true .
+  ?s any:hasText ?text .
+} WHERE {
+  ?s a knora-api:Resource .
+  ?s a any:Thing .
+  OPTIONAL {
+    ?s any:hasText ?text .
+    any:hasText knora-api:objectType xsd:string .
+    ?text a xsd:string .
+  }
+}`;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.resources$ = this.http
+      .get<Thing[]>(
+        environment.api +
+          '/v2/searchextended/' +
+          encodeURIComponent(this.gravQuery)
+      )
+      .pipe(
+        // tap(console.log),
+        map(
+          jsonld =>
+            ConvertJSONLD.createReadResourcesSequenceFromJsonLD(jsonld)
+              .resources
+        ),
+        mergeAll(),
+        // tap(console.log),
+        map(
+          (res: ReadResource) =>
+            ({
+              id: res.id,
+              text: '?' // FIXME: get hasText value from resource
+            } as Thing)
+        ),
+        toArray()
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { KuiCoreModule } from '@knora/core';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    KuiCoreModule.forRoot({
+      api: environment.api,
+      media: environment.media,
+      app: environment.app
+    }),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
